c303b3aeb84a86d36422bae0e5ebbd66a59fbc61
gr8geek <pratyush9000@gmail.com>
2020-10-28 17:49:12 +0530

    search icons :)

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAHQwYgkB2mm64XqBKSyu9zDzuMh7oQIe4",
+    authDomain: "clone-86b57.firebaseapp.com",
+    databaseURL: "https://clone-86b57.firebaseio.com",
+    projectId: "clone-86b57",
+    storageBucket: "clone-86b57.appspot.com",
+    messagingSenderId: "952955561251",
+    appId: "1:952955561251:web:94059468464e9b5aca5896",
+    measurementId: "G-LLDJNTNRJD"
+  };
